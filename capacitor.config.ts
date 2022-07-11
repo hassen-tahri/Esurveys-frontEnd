773c066fcfe523c073fcd0db0bdf49dfc873da01
hassentahri40 <hassentahri40@gmail.com>
2022-07-12 00:09:25 +0100

    init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.esurveys.app',
+  appName: 'esurveys',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
